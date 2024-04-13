058dafcb458a1913dbac1b29e42fb0b1776f920d
riyonrichi46 <riyonrichi46@gmail.com>
2024-04-13 14:01:40 +0700

    add contact

--- main.js
+++ main.js
@@ -6,9 +6,28 @@ function toggleDetail(e) {
     } else {
         $(target).html("Less Info").addClass("active")
     }
-
     const item = $(target).parents(".about-exp-item")
     const detail = $(item).children(".about-exp-item-detail")
-
     $(detail).slideToggle()
+}
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+   if(!$(email).val()) {
+    alert("email is required")
+   } else if (!$(subject).val()) {
+    alert("subject is required")
+   } else if (!$(message).val()) {
+    alert("message is required")
+   } else {
+    alert("form submitted")
+    $(email).val("")
+    $(subject).val("")
+    $(message).val("")
+    
+   }
 }
